83b50879dc1922d3ab96de77387e2021c7fd2864
Michael DiPasquale <mdipasquale315@gmail.com>
2017-09-16 22:51:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return 'Happy holidays, you!'
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
-}
-function holidayCountdown(holiday, days) {
-  return `It's ${days}days until ${holiday}!`
+  return `Happy ${holiday}, ${name}!`
 }
